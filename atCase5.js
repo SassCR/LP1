651e87ca91bdb8973566699205a9d4a7b14f8b41
SassCR <renefodao123@gmail.com>
2026-03-04 20:19:57 -0400

    Add files via upload

---
+++ atCase5.js
@@ -0,0 +1,31 @@
+let ddd = 123
+switch (ddd) {
+    case 67:
+        console.log("MS");
+        
+        break;
+    case 77:
+        console.log("algum lugar");
+        
+        break;
+    case 61:
+        
+        break;
+    case :
+        
+        break;
+    case :
+        
+        break;
+    case :
+        
+        break;
+    case :
+        
+        break;
+
+    default:
+        console.log("Ele não é do brasil.");
+        
+        break;
+}
---
+++ atdowhilie10.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")()
+
+let numero
+
+
+do{
+    numero = Number(prompt("Digite o número: "))//Faça 
+}while (numero != 0) {//enqaunto não é zero
+    
+}
+//
