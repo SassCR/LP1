f4d9b09bcc188d904beb7c8ee7ac8c441066c1c5
SassCR <renefodao123@gmail.com>
2026-03-10 21:01:40 -0400

    Add files via upload

---
+++ criandoapi.js
@@ -0,0 +1,4 @@
+const { soma }  = require("./matematica"); // ./dizer onde está o arquivo e a pasta.
+
+let res = soma(1, 2);
+console.log(res);
---
+++ matematica.js
@@ -0,0 +1,7 @@
+function soma(a, b) {
+    return a + b
+}
+function mul(a, b) {
+    return a * b
+}
+module.exports = {soma, mul} //Para pode usar essas funçãos em outros arquivos
