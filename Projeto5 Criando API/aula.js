4db7924d51f2025e487368dfb03f0164306edf08
SassCR <Renefodao123@gmail.com>
2026-04-15 20:16:01 -0400

    Atualização

--- Projeto5 Criando API/aula.js
+++ Projeto5 Criando API/aula.js
@@ -15,27 +15,28 @@ const porta = 13957
 //U = UPDATE - Atualizar dados de uma postagem/pessoa.(PUT/PATCH)
 //D = DELETE - Remover uma pessoa/foto/post.(DELETE)
 
-
+//A diferença entre PUT e PATCH é que o Put ele solicita todos os dados, já p patch, só um dado
 
 
 let pessoas = [
-    {nome: "João", idade: 19},
-    {nome: "Maria", idade: 23},
-    {nome: "Pedro", idade: 65},
-    {nome: "Amanda", idade: 34}
+    { nome: "João", idade: 19 },
+    { nome: "Maria", idade: 23 },
+    { nome: "Pedro", idade: 65 },
+    { nome: "Amanda", idade: 34 }
 ]
 
 //---------------------------------------------------CRUD pessoas -----------------------------
 //Read do CRUD
 api.get('/pessoa', (requisicao, resposta) => {
-resposta.send(pessoas);
+    resposta.send(pessoas);
 })
+//=================================================================================================
 //Create do CRUD                                 //Requisição é o usuario que envia, resposta é quando ele solicita algo
 api.post('/pessoa', (requisicao, resposta) => { //O primeiro sempre será requisição e o outro resposta, não importa o nome, só que sempre será assim.
-   //capturando dados enviados na requisição.
+    //capturando dados enviados na requisição.
     const novaPessoa = requisicao.body;
     //na linha 37 estamos fazendo uma verificação para saber se o usuario ao fazer a requisição enviou nome e a idade da pessoa.
-    if(!novaPessoa.nome || !novaPessoa.idade){
+    if (!novaPessoa.nome || !novaPessoa.idade) {
         return resposta.send("ERRO! Informe o nome e a idade.")
     }
     //Caso tenha enviado tudo certinho vai executar a linha 41 em diante.
@@ -43,17 +44,32 @@ api.post('/pessoa', (requisicao, resposta) => { //O primeiro sempre será requis
     pessoas.push(novaPessoa);
     resposta.send("SUCESSO! Pessoa cadastrada!")
 })
+//=================================================================================================
 //Delete do CRUD
-api.delete("/pessoa", (requisicao, resposta)=> {
+api.delete("/pessoa", (requisicao, resposta) => {
     const pessoaParaDeletar = requisicao.body
     //simulando que as pessoas tem ID no BD (Vetor pessoas)
-    if(!pessoaParaDeletar.id){
+    if (!pessoaParaDeletar.id) {
         return resposta.send("ERRO! Informe o ID!")
     }
     //Removendo do vetor pessoa a pessoa do indice passado como ID
-    pessoas.splice(pessoaParaDeletar.id, 1)//Esse comando remover coisas do vetor
+    pessoas.splice(pessoaParaDeletar.id, 1)//Esse comando remover coisas do vetor/Caso ele de erro nesssa linha de codigo, ele vai quebra o resto e não vai continuar
     resposta.send("SUCESSO! Pessoa removida!")
 })
+//=================================================================================================
+//Update do CRUD
+api.put("/pessoa", (requisicao, resposta) => {
+    const pessoaParaEditar = requisicao.body; //Sempre é necessário caso deseje mexer em algo.
+
+    if (!pessoaParaEditar.id || !pessoaParaEditar.nome || !pessoaParaEditar.idade) {
+        return  resposta.send("ERRO!: Informe as seguintes informaçãos nome, idade e ID!");
+    }
+    pessoas[pessoaParaEditar.id] = pessoaParaEditar;
+    resposta.send("PESSOA EDITADA COM SUCESSO!")
+})
+
+
+
 //api.get ele só vai aceitar ler e puxar informações.
 api.get("/Status", (req, res) => { //req= requisição. res= resposta, no / Precisa ter algo para localizar a API na localHost.
     res.send("API tá on.")//Enviando uma resposta usando o res..
@@ -62,3 +78,5 @@ api.get("/Status", (req, res) => { //req= requisição. res= resposta, no / Prec
 api.listen(porta, () => { //Eu posso colocar apenas o número da porta sem a necessidade de criar uma variavel.
     console.log(`API tá rodando na porta ${porta}`);
 })
+
+//Caso eu queira usar essa api vou precisar usar o cors.
